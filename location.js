b180ba793a53daeeef8776e7cf505850df372005
John Sattari <johnsattari@Johns-MacBook-Air.local>
2020-06-10 19:45:01 -0700

    updated index.html to remove inline-scripts. updated location.js to use xmlhttpRequests instead of google maps api method. change content security policy in manifest

--- location.js
+++ location.js
@@ -1,16 +1,40 @@
 // var button = document.getElementById("location-button");
-// var x = document.getElementById("drop_loc");
+//var x = document.getElementById('drop_loc');
 
-$('#location-button').click(function(){
+function httpGet(theUrl) {
+
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, true ); // false for synchronous request
+    xmlHttp.responseType = '';
+    xmlHttp.onload = function () {
+      if (xmlHttp.readyState === xmlHttp.DONE) {
+          if (xmlHttp.status === 200) {
+              console.log(xmlHttp.response);
+          }
+      }
+    };
+    xmlHttp.send(null);
+    //return xmlHttp.responseText;
+};
+
+function main() {
+  const key = 'QjBE85QzuwMz0zbwcxl609zYM3BM3VA5';
+  
   if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position){
-        console.log(position);
-        $.get("http://open.mapquestapi.com/geocoding/v1/reverse?key=QjBE85QzuwMz0zbwcxl609zYM3BM3VA5&location="+ position.coords.latitude + "," + position.coords.longitude +"&includeRoadMetadata=true&includeNearestIntersection=true", function(data) {
-          console.log(data);
-        })
-        var img = new Image();
-        img.src = "http://open.mapquestapi.com/geocoding/v1/reverse?key=QjBE85QzuwMz0zbwcxl609zYM3BM3VA5&location=" + position.coords.latitude + "," + position.coords.longitude + "&includeRoadMetadata=true&includeNearestIntersection=true&thumbMaps=true";
-        $('#drop_loc').html(img);
-      });
-  }
+    navigator.geolocation.getCurrentPosition(function(position){
+      console.log(position);
+      
+      const url = 'http://open.mapquestapi.com/geocoding/v1/reverse?key=' + key + '&location='+ position.coords.latitude + ',' + position.coords.longitude + '&outFormat=json';
+      
+      console.log(httpGet(url));
+    });
+  };
+};
+
+function clickHandler(e) {
+  setTimeout(main, 1000);
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('button').addEventListener('click', clickHandler);
 });
