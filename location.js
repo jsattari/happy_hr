c1f7e14b1c7b260e7e26dc9e40019b5b0671dfe7
John Sattari <johnsattari@Johns-MacBook-Air.local>
2020-06-21 15:30:19 -0700

    added innerHTML to insert location

--- location.js
+++ location.js
@@ -3,32 +3,36 @@
 
 function httpGet(theUrl) {
 
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, true ); // false for synchronous request
-    xmlHttp.responseType = '';
-    xmlHttp.onload = function () {
-      if (xmlHttp.readyState === xmlHttp.DONE) {
-          if (xmlHttp.status === 200) {
-              console.log(xmlHttp.response);
-          }
-      }
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open( "GET", theUrl, true ); // false for synchronous request
+  xmlHttp.responseType = '';
+  xmlHttp.onload = function () {
+    if (xmlHttp.readyState === xmlHttp.DONE) {
+        if (xmlHttp.status === 200) {
+          var junk = xmlHttp.response;
+          var shiny = JSON.parse(junk);
+          var shinier = shiny.results['0'].locations['0'];
+          var street = shinier.street;
+          var city = shinier.adminArea5;
+          document.getElementById('map').innerHTML = 'Location: ' + street + ', ' + city;
+        };
+      };
     };
     xmlHttp.send(null);
-    //return xmlHttp.responseText;
 };
 
 function main() {
-  const key = 'QjBE85QzuwMz0zbwcxl609zYM3BM3VA5';
-  
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position){
-      console.log(position);
-      
-      const url = 'http://open.mapquestapi.com/geocoding/v1/reverse?key=' + key + '&location='+ position.coords.latitude + ',' + position.coords.longitude + '&outFormat=json';
-      
-      console.log(httpGet(url));
-    });
-  };
+const key = 'QjBE85QzuwMz0zbwcxl609zYM3BM3VA5';
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position){
+    // console.log(position);
+    
+    const url = 'http://open.mapquestapi.com/geocoding/v1/reverse?key=' + key + '&location='+ position.coords.latitude + ',' + position.coords.longitude + '&outFormat=json';
+    
+    console.log(httpGet(url));
+  });
+};
 };
 
 function clickHandler(e) {
